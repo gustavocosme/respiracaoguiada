4f6a37be770da8ecf00730f4c243162a073749fe
Gustavo Cosme <gustavo_cosme@hotmail.com>
2025-07-01 15:00:07 -0300

    Aula 1 - Create project

---
+++ respiracaoguiada/respiracaoguiada Watch App/respiracaoguiadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  respiracaoguiadaApp.swift
+//  respiracaoguiada Watch App
+//
+//  Created by Gustavo Cosme on 01/07/25.
+//
+
+import SwiftUI
+
+@main
+struct respiracaoguiada_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ respiracaoguiada/respiracaoguiada/respiracaoguiadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  respiracaoguiadaApp.swift
+//  respiracaoguiada
+//
+//  Created by Gustavo Cosme on 01/07/25.
+//
+
+import SwiftUI
+
+@main
+struct respiracaoguiadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
